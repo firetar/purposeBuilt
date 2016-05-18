c28a5df5ea085a5c0cbd43a3b4f38994f33782f7
Mike <micky.lives@gmail.com>
2016-05-18 20:12:46 +0200

    sorry

---
+++ PurposeBuilt/app/src/main/java/richard/mike/com/purposebuilt/SignUp.java
@@ -0,0 +1,17 @@
+package richard.mike.com.purposebuilt;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by Michael on 10/05/2016.
+ */
+public class SignUp extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+    }
+}
